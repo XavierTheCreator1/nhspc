c8edd425418ddb4940c9eabf6837a0720a9a87f5
X <xavierthecreator1@gmail.com>
2023-12-22 21:27:09 -0500

    update 5 files

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-console.log("starting app..");
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  response.write("Hello World!");
+  response.end();
+});
+
+server.listen(5000);
