1df90acc633e5dcf146d3e07a1d27951956743a4
X <xavierthecreator1@gmail.com>
2023-12-24 12:07:01 -0500

    update 1 file and delete 1 file

--- src/app.js
+++ src/app.js
@@ -3,12 +3,7 @@ const path = require("path");
 
 const app = express();
 
-// setup static and middleware
 app.use(express.static("./public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./home/index.html"));
-});
 
 app.all("*", (req, res) => {
   res.status(404).send("resource not found");
