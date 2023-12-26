164262fdfb2020512ce4f9280f73f1bf8ca5d4b9
X <xavierthecreator1@gmail.com>
2023-12-26 04:01:07 -0500

    update 5 files and delete 22 files

---
+++ nhspc-site/app/api/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ApiPage = () => {
+  return <h1>Welcome to the NHSPC API!</h1>;
+};
+
+export default ApiPage;
--- nhspc-site/tailwind.config.ts
+++ nhspc-site/tailwind.config.ts
@@ -11,13 +11,6 @@ const config: Config = {
       gridTemplateColumns: {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
-      colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
-        },
-      },
     },
     keyframes: {
       shimmer: {
@@ -27,6 +20,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), require('daisyui')],
 };
 export default config;
