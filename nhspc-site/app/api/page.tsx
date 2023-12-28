bbbf3fc39eb4d1d7efbaf2a70034e3b2aed44754
I-Make-Gamez <90048664+I-make-gamez@users.noreply.github.com>
2023-12-27 22:13:13 -0500

    update 6 files

--- nhspc-site/app/api/page.tsx
+++ nhspc-site/app/api/page.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import Link from 'next/link';
+'use client';
 
-const API = () => {
+import React, { useEffect } from 'react';
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
+const ApiRequestComponent: React.FC = () => {
+  // Replace the URL with the endpoint you want to make a GET request to
+  const apiUrl: string = 'http://localhost:5000/api/createReminder';
+
+  const queryParams = {
+    key: '0SrJgD50TofGSHGi1ZsKoS1XHJwuX9yDMRjyUDptR4PMTYrUOs',
+    date: '2023',
+    topic: 'Test reminder for development purposes',
+  };
+
+  useEffect(() => {
+    // Make a GET request using Axios
+    axios
+      .get(apiUrl, {
+        params: queryParams,
+      })
+      .then((response: AxiosResponse) => {
+        // Handle the successful response
+        console.log('Response data:', response.data);
+      })
+      .catch((error: AxiosError) => {
+        // Handle errors
+        console.error('Error making GET request:', error.message);
+      });
+  }, []); // Empty dependency array means this effect runs once after the component mounts
+
   return (
-    <main>
-      This is the nhspc api used for sending post requests to the nhps discord
-      bot, whiz
-    </main>
+    <div>
+      <p>Check the browser's console for the api data</p>
+      {/* You can include JSX elements and components as needed */}
+    </div>
   );
 };
 
-export default API;
+export default ApiRequestComponent;
--- whiz/src/index.js
+++ whiz/src/index.js
@@ -25,8 +25,69 @@ client.once("ready", () => {
   });
 });
 
-// client.on(Events.MessageCreate, async (msg) => {
-//   msg.channel.send("Hello World!");
-// });
+// postgres
+// app.js
+const postgres = require("postgres");
+require("dotenv").config();
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID, apikey } =
+  process.env;
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: "require",
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
+
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log(result);
+}
+
+getPgVersion();
+
+//Start a webserver to listen for api requests
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+app.get("/api/createReminder", async (req, res) => {
+  console.log(req.query.key == apikey);
+  if (req.query.key != apikey) {
+    res.status(403).send("Forbidden");
+  } else {
+    if (req.query.date == undefined || req.query.topic == undefined) {
+      res.status(400).send("-1");
+    } else {
+      const result = await sql`
+          INSERT INTO r (date, topic)
+          VALUES (${req.query.date}, ${req.query.topic})
+          RETURNING *`; // Assuming 'reminders' is your table name
+      res.status(202).json(result);
+    }
+  }
+});
+
+app.get("/api", (req, res) => {
+  res.status(403).send("Forbidden");
+});
+
+app.get("/api/*", (req, res) => {
+  res.status(404).send("The server responded with a status of 404");
+});
 
 client.login(process.env.token);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
