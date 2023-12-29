ad581ee485abccafdfbabefbb097f7392d9dbcd1
X <xavierthecreator1@gmail.com>
2023-12-29 01:21:36 -0500

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,9 @@
-"use client";
-
 import React from "react";
 import "@/app/components/global.css";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  const theme = React.useRef("night");
-  React.useEffect(() => {
-    const userTheme = window.matchMedia(
-      "(prefers-color-scheme: light)"
-    ).matches;
-    theme.current = userTheme ? "night" : "fantasy";
-    console.log(userTheme);
-  }, []);
-
   return (
-    <html lang="en" data-theme={theme}>
+    <html lang="en" data-theme="night">
       <body>{children}</body>
     </html>
   );
