fbdd7a9dbbc5d002812f137f7de4c7cbb283a256
X <xavierthecreator1@gmail.com>
2024-01-02 17:24:46 -0500

    update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Link from "next/link";
-import ChangingTip from "./components/error/ChangingTip";
+import ChangingTip from "@/app/components/error/ChangingTip";
 
 const NotFoundPage = () => {
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center">
-      <h1 className="mb-5 text-8xl text-error">Error 404</h1>
-      <h2 className="mb-10 text-6xl">Page Not Found</h2>
+      <h1 className="mb-5 text-6xl sm:text-8xl text-error text-center">
+        Error 404
+      </h1>
+      <h2 className="mb-10 text-4xl text-center">Page Not Found</h2>
       <ChangingTip />
       <Link href="/">
         <button className="btn btn-primary btn-wide">Back Home</button>
